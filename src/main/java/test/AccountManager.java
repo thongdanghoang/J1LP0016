66a3969944927a2773b898c960fb4f77a9dc5e11
Thống Đặng Hoàng <thongdh3401@gmail.com>
2022-11-10 13:50:01 +0700

    add AccountManager's `create` function

--- src/main/java/test/AccountManager.java
+++ src/main/java/test/AccountManager.java
@@ -13,6 +13,17 @@ public class AccountManager {
             System.out.print(accountDTO.getUsername().concat("@J1.L.P0016:~$ "));
             String cmd = Input.getString();
             if (cmd.toLowerCase().equals("create")){
+                System.out.print("Input username: ");
+                String username = Input.getString();
+                System.out.print("Input password: ");
+                String password = Input.getString();
+                System.out.print("Input role: ");
+                String role = Input.getString();
+                try {
+                    service.create(new AccountDTO(username,password,role));
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
             }
             if (cmd.toLowerCase().equals("read")){
                 System.out.println("loading...");
